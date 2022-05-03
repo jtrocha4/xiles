05dbf1c3bbd35233d589c7d25b8ec02d6faf0f6c
jtrocha4 <jtrocha04@gmail.com>
2022-05-02 23:13:21 -0500

    nav

---
+++ src/componentes/MenuNavegacion.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+function MenuNavegacion() {
+    return (
+        <div>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+                <div className='container-fluid'>
+                <span class="navbar-brand mb-0 h1">Xiles´s</span>
+                <ul className='navbar-nav'>
+                    <li className='nav-item'><a href="" className='nav-link'>Hombre</a></li>
+                    <li className='nav-item'><a href="" className='nav-link'>Mujer</a></li>
+                    <li className='nav-item'><a href="" className='nav-link'>Tiendas</a></li>
+                </ul>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default MenuNavegacion
