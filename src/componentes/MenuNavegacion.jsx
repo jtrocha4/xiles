ef77d72816556e4a79ac958b3a85addbcc2190ed
jtrocha4 <jtrocha04@gmail.com>
2022-05-05 21:56:43 -0500

    v2

--- src/componentes/MenuNavegacion.jsx
+++ src/componentes/MenuNavegacion.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
-
 function MenuNavegacion() {
     return (
         <div>
             <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
                 <div className='container-fluid'>
-                <span class="navbar-brand mb-0 h1">Xiles´s</span>
-                <ul className='navbar-nav'>
-                    <li className='nav-item'><a href="" className='nav-link'>Hombre</a></li>
-                    <li className='nav-item'><a href="" className='nav-link'>Mujer</a></li>
-                    <li className='nav-item'><a href="" className='nav-link'>Tiendas</a></li>
-                </ul>
+                    <a className='navbar-brand mb-0 h1' href="">Xiles´s</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#firstNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div className='collapse navbar-collapse' id='firstNavbar'>
+                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                            <li className='nav-item'><a className='nav-link' href="">Hombre</a></li>
+                            <li className='nav-item'><a className='nav-link' href="">Mujer</a></li>
+                            <li className='nav-item'><a className='nav-link' href="">Tiendas</a></li>
+                        </ul>
+                        <form className='d-flex' action="">
+                            <input className='form-control form-control-sm form-control me-1' type="text" name="" id="" placeholder='Buscar' />
+                            <button className='btn btn-outline-light me-2'><i className="bi bi-search"></i></button>
 
+                            {/* Boton de bolsa */}
+                            <button className='btn btn-outline-light me-2'><i className="bi bi-bag"></i></button>
+                            {/* Boton de Cuenta */}
+                            <button className='btn btn-outline-light'><i className="bi bi-person"></i></button>
+                        </form>
+                    </div>
                 </div>
             </nav>
+
         </div>
     )
 }
